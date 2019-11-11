d7c5087628d149c5a3e7ee5973bef69fd12009b5
gonzalo.acuna <teddytpc1@gmail.com>
2019-11-11 15:53:46 -0300

    se modifica el script de agregado de manejado

--- OracleWebLogic/dockerfiles/10.3.6/container-scripts/check.py
+++ OracleWebLogic/dockerfiles/10.3.6/container-scripts/check.py
@@ -1,7 +1,9 @@
-connect('weblogic','welcome1','t3://172.18.0.2:7001')
+connect('weblogic','welcome1','t3://wlsadmin:7001')
 ms_name = sys.argv[1]
 ms_listen_address = sys.argv[2]
 ms_cluster_name = sys.argv[3]
+ms_listen_port = sys.argv[4]
+
 print '===> Server \"' +ms_name+'\" s'
 
 
@@ -19,27 +21,25 @@ except:
  startEdit()
  try:
      cd('/Clusters/' + ms_cluster_name)
-     # Create Cluster
-     print '===> Cluster \"' +ms_cluster_name+'\" already exists'
-     print '===> No action was performed'
-     exit()
+ # Create Cluster
+ print '===> Cluster \"' +ms_cluster_name+'\" already exists'
+ print '===> No action was performed'
+ exit()
  except:
-     pass
-     cd('/')
-     create(ms_cluster_name,'Cluster')
+  pass
+  cd('/')
+  create(ms_cluster_name,'Cluster')
 
-     # Create MS
-     print '===> Creating MS ' + ms_name
-     cd('/')
-     create(ms_name,'Server')
+  # Create MS
+  print '===> Creating MS' + ms_name'
+  cd('/')
+  create(ms_name,'Server')
 
-     cd('/Servers/' + ms_name)
-     set('ListenPort', '7101')
-     set('ListenAddress', ms_listen_address)
-     cmo.setCluster(getMBean('/Clusters/'+ms_cluster_name))
+  cd('/Servers/' + ms_name)
+  set('ListenPort', ms_listen_port)
+  set('ListenAddress', ms_listen_address)
+  assign('Server',ms_name,'Cluster',ms_cluster_name)
 
 
-
- save()
- activate()
-
+  save()
+  activate()
--- OracleWebLogic/dockerfiles/10.3.6/container-scripts/check.sh
+++ OracleWebLogic/dockerfiles/10.3.6/container-scripts/check.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
-. /u01/oracle/weblogic/wlserver_10.3/server/bin/setWLSEnv.sh
 export ms_name=$1
 export ms_listen_address=`echo "${ms_name,,}"`
 export ms_listen_address=`echo "${ms_listen_address/_/}"`
 export ms_listen_address=`echo "${ms_listen_address/1/}"`
 export ms_cluster_name=`echo "${ms_name/WLS/CLUSTER}"`
 export ms_cluster_name=`echo "${ms_cluster_name/1/}"`
-
+export ms_listen_port=$2
 
 echo $ms_name
 echo $ms_listen_address
 echo $ms_cluster_name
-java weblogic.WLST /u01/oracle/weblogic/wlserver_10.3/common/bin/check.py $ms_name $ms_listen_address $ms_cluster_name
+java weblogic.WLST check.py $ms_name $ms_listen_address $ms_cluster_name $ms_listen_port
+
+docker exec -it wlsadmin mkdir /u01/oracle/weblogic/user_projects/base_domain/servers/$ms_name
+docker exec -it wlsadmin cp /u01/oracle/weblogic/user_projects/base_domain/servers/AdminServer/security/boot.properties /u01/oracle/weblogic/user_projects/base_domain/servers/$ms_name/security
